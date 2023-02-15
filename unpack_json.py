a871ccba628c51330780296f024bb9a399f03e16
Rubén <74114026+rubzip@users.noreply.github.com>
2023-02-15 21:45:49 +0100

    Add files via upload

--- unpack_json.py
+++ unpack_json.py
@@ -1,14 +1,20 @@
 import pandas as pd
 from dateutil.parser import parse
 
-def is_date(code)->bool:
+def is_date(code:str)->bool:
+    """
+    Takes a code and returns if it's a date.
+    """
     try:
         parse(code, fuzzy=False)
         return True
     except:
         return False
     
-def return_type(value):
+def return_type(value)->str:
+    """
+    Takes a value and returns the date type (including date)
+    """
     val_type = type(value).__name__
     if value is None:
         return '???'
@@ -19,7 +25,16 @@ def return_type(value):
     else:
         return val_type
 
-def get_tree(father:dict, father_name="", depth=0):
+def get_tree(father:dict, father_name="", depth=0)->list:
+    """
+    Recursive function. Takes a dictionary and returns the tree structure
+    Returns a list of lists:
+        Each row contains 4 values:
+            * Father_name: the name of the previous node
+            * son_name: current node
+            * son_type: data type stored in the node
+            * depth: depth of the current node
+    """
     data = []
     for son_name in father.keys():
         son = father[son_name]
@@ -33,7 +48,10 @@ def get_tree(father:dict, father_name="", depth=0):
                     data += get_tree(son[0], son_name, depth+1)
     return data
 
-def write_tree(tree):
+def write_tree(tree:list)->str:
+    """
+    returns a str with the tree structure like tree command in bash
+    """
     file = ""
     for node in tree:
         name, dtype, depth = node[1:4]
